af8d4e4afdcbd906b8c2cc2ce4220ecc8a2b39c5
Nely Victória Oliveira da Silva <o.nely@escolar.ifrn.edu.br>
2026-06-01 14:02:15 -0300

    nhe

---
+++ TCC26/Assets/Scripts/Core/LogoPulse.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LogoPulse : MonoBehaviour
+{
+    void OnEnable()  => RhythmManager.OnBeatStatic += Pulse;
+    void OnDisable() => RhythmManager.OnBeatStatic -= Pulse;
+
+    void Pulse() => StartCoroutine(ScaleBounce());
+
+    IEnumerator ScaleBounce() {
+        float t = 0;
+        while (t < 0.12f) {
+            t += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(Vector3.one * 1.08f, Vector3.one, t / 0.12f);
+            yield return null;
+        }
+        transform.localScale = Vector3.one;
+    }
+}
